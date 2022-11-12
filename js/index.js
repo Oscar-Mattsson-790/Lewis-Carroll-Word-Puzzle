17f7fc9efe1455afa1cfe3349e9458eff7faa709
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-11-12 16:34:31 -0600

    ongoing

--- js/index.js
+++ js/index.js
@@ -1,58 +1,63 @@
 "use strict";
 
-// Word Puzzle
-let words = [
-  {
-    startWord: "FOUR",
-    endWord: "FIVE",
-  },
-  {
-    startWord: "EYE",
-    endWord: "LID",
-  },
-  {
-    startWord: "TIGER",
-    endWord: "ROSES",
-  },
-  {
-    startWord: "WHEAT",
-    endWord: "BREAD",
-  },
-];
+const wordCombinations = {
+  combo1: ["FOUR", "FIVE"],
+  combo2: ["EYE", "LID"],
+  combo3: ["TIGER", "ROSES"],
+  combo4: ["WHEAT", "BREAD"],
+};
 
-let word = randomizeWord();
-let input = document.querySelector("#inputText");
-let newWord = "";
+const button = document.querySelector(".btn");
+const startWord = document.querySelector(".startWord");
+const endWord = document.querySelector(".endWord");
+const inputBox = document.querySelector(".inputBox");
 
-function randomizeWord() {
-  let index = Math.floor(Math.random() * words.length);
-  return words[index];
+function getRandomCombo() {
+  let randomNumber = Math.floor(Math.random() * 4) + 1;
+  let numberOfLetters;
+
+  if (randomNumber === 1) {
+    numberOfLetters = wordCombinations.combo1[0].length;
+
+    let inputField = document.createElement("input");
+    inputField.innerHTML = "Hello";
+    inputBox.appendChild(inputField);
+    console.log(inputField.innerText);
+
+    startWord.textContent = `${wordCombinations.combo1[0]} (${numberOfLetters})`;
+    endWord.textContent = `${wordCombinations.combo1[1]} (${numberOfLetters})`;
+
+    console.log("starWord: ", wordCombinations.combo1[0]);
+    console.log("endWord: ", wordCombinations.combo1[1]);
+  } else if (randomNumber === 2) {
+    numberOfLetters = wordCombinations.combo2[0].length;
+
+    startWord.textContent = `${wordCombinations.combo2[0]} (${numberOfLetters})`;
+    endWord.textContent = `${wordCombinations.combo2[1]} (${numberOfLetters})`;
+
+    console.log("startWord: ", wordCombinations.combo2[0]);
+    console.log("endWord: ", wordCombinations.combo2[1]);
+  } else if (randomNumber === 3) {
+    numberOfLetters = wordCombinations.combo3[0].length;
+
+    startWord.textContent = `${wordCombinations.combo3[0]} (${numberOfLetters})`;
+    endWord.textContent = `${wordCombinations.combo3[1]} (${numberOfLetters})`;
+
+    console.log("startWord: ", wordCombinations.combo3[0]);
+    console.log("endWord: ", wordCombinations.combo3[1]);
+  } else {
+    numberOfLetters = wordCombinations.combo4[0].length;
+
+    startWord.textContent = `${wordCombinations.combo4[0]} (${numberOfLetters})`;
+    endWord.textContent = `${wordCombinations.combo4[1]} (${numberOfLetters})`;
+
+    console.log("startWord: ", wordCombinations.combo4[0]);
+    console.log("endWord: ", wordCombinations.combo4[1]);
+  }
 }
 
-document.querySelector(".btn").addEventListener("click", function () {
-  word = randomizeWord();
-  document.querySelector(".startWord").innerHTML = word.startWord;
-  document.querySelector(".endWord").innerHTML = word.endWord;
-});
+getRandomCombo();
 
-input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" && input.value.length === word.startWord.length) {
-    newWord = input.value;
-    console.log(newWord);
-    let diff = 0;
-    for (let i = 0; i < input.value.length; i++) {
-      if (input.value[i] !== word.startWord.length) {
-        diff++;
-      }
-    }
-    if (diff === 1) {
-      //fetch funckiton
-    } else {
-      console.log();
-    }
-  } else {
-    console.log("Wrong");
-  }
+button.addEventListener("click", function () {
+  getRandomCombo();
 });
-
-async function gameEngine() {}
