f9cae9614134eee11e56c976d9eaa80dfd60c3f2
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-11-13 15:20:10 -0600

    Ongoing
    Ongoing

--- js/index.js
+++ js/index.js
@@ -12,52 +12,61 @@ const startWord = document.querySelector(".startWord");
 const endWord = document.querySelector(".endWord");
 const inputBox = document.querySelector(".inputBox");
 
+function creatInputFields(numberOfLetters, word) {
+  console.log("första ordet i kombon: ", word[0]);
+
+  for (let i = 0; i < word.length; i++) {
+    console.log(i);
+    let inputField = document.createElement("input");
+    inputField.placeholder = word[i];
+    inputBox.appendChild(inputField);
+  }
+}
+
+function removeInput() {
+  let inputsFields = document.querySelectorAll("input");
+  console.log("Hur lång är inputfield: ", inputsFields.length);
+
+  for (const input of inputsFields) {
+    console.log(input);
+    inputBox.removeChild(inputBox.firstChild);
+  }
+}
+
 function getRandomCombo() {
   let randomNumber = Math.floor(Math.random() * 4) + 1;
   let numberOfLetters;
 
   if (randomNumber === 1) {
     numberOfLetters = wordCombinations.combo1[0].length;
+    creatInputFields(numberOfLetters, wordCombinations.combo1[0]);
 
-    let inputField = document.createElement("input");
-    inputField.innerHTML = "Hello";
-    inputBox.appendChild(inputField);
-    console.log(inputField.innerText);
-
-    startWord.textContent = `${wordCombinations.combo1[0]} (${numberOfLetters})`;
-    endWord.textContent = `${wordCombinations.combo1[1]} (${numberOfLetters})`;
-
-    console.log("starWord: ", wordCombinations.combo1[0]);
-    console.log("endWord: ", wordCombinations.combo1[1]);
+    startWord.textContent = `${wordCombinations.combo1[0]}`;
+    endWord.textContent = `${wordCombinations.combo1[1]}`;
   } else if (randomNumber === 2) {
     numberOfLetters = wordCombinations.combo2[0].length;
+    creatInputFields(numberOfLetters, wordCombinations.combo2[0]);
 
-    startWord.textContent = `${wordCombinations.combo2[0]} (${numberOfLetters})`;
-    endWord.textContent = `${wordCombinations.combo2[1]} (${numberOfLetters})`;
-
-    console.log("startWord: ", wordCombinations.combo2[0]);
-    console.log("endWord: ", wordCombinations.combo2[1]);
+    startWord.textContent = `${wordCombinations.combo2[0]}`;
+    endWord.textContent = `${wordCombinations.combo2[1]}`;
   } else if (randomNumber === 3) {
     numberOfLetters = wordCombinations.combo3[0].length;
+    creatInputFields(numberOfLetters, wordCombinations.combo3[0]);
 
-    startWord.textContent = `${wordCombinations.combo3[0]} (${numberOfLetters})`;
-    endWord.textContent = `${wordCombinations.combo3[1]} (${numberOfLetters})`;
-
-    console.log("startWord: ", wordCombinations.combo3[0]);
-    console.log("endWord: ", wordCombinations.combo3[1]);
+    startWord.textContent = `${wordCombinations.combo3[0]}`;
+    endWord.textContent = `${wordCombinations.combo3[1]}`;
   } else {
     numberOfLetters = wordCombinations.combo4[0].length;
+    creatInputFields(numberOfLetters, wordCombinations.combo4[0]);
 
-    startWord.textContent = `${wordCombinations.combo4[0]} (${numberOfLetters})`;
-    endWord.textContent = `${wordCombinations.combo4[1]} (${numberOfLetters})`;
-
-    console.log("startWord: ", wordCombinations.combo4[0]);
-    console.log("endWord: ", wordCombinations.combo4[1]);
+    startWord.textContent = `${wordCombinations.combo4[0]}`;
+    endWord.textContent = `${wordCombinations.combo4[1]}`;
   }
 }
 
 getRandomCombo();
 
 button.addEventListener("click", function () {
+  removeInput();
   getRandomCombo();
 });
